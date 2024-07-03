8f39593ad31f7a1eff4bd485ff5fe473606b3b96
Seng Seang Leng <seangleng507@gmail.com>
2024-07-03 09:11:54 +0700

    adding card and fix bugs

--- src/components/Alert/AlertCompo.tsx
+++ src/components/Alert/AlertCompo.tsx
@@ -1,17 +1,50 @@
-import React from "react";
+'use client';
+import React, {useEffect, useState} from "react";
 import { AlertProps } from "@/types/Types";
-import Container from "../container-section/Container";
 import {Alert} from "antd";
 
 export default function AlertCompo({ title, content, variant }: AlertProps) {
+  
+  const [visible, setVisible] = useState<boolean>(true);
+  const [timeLeft, setTimeLeft] = useState<number>(0);
+  
+  useEffect(() => {
+    const storedTime = localStorage.getItem('bannerCloseTime');
+    const currentTime = Date.now();
+    
+    if (storedTime) {
+      const closeTime = parseInt(storedTime, 10);
+      const timeToShowAgain = closeTime + (1000 * 60 * 60);
+      if (currentTime >= timeToShowAgain) {
+        setVisible(true);
+        localStorage.removeItem('bannerCloseTime');
+      } else {
+        const diff = timeToShowAgain - currentTime;
+        setTimeLeft(Math.round(diff / 1000));
+      }
+    }
+  }, []);
+  
+  const handleClose = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setVisible(false);
+    const currentTime = Date.now();
+    localStorage.setItem('bannerCloseTime', String(currentTime));
+  };
+  
   return (
-    <Alert
-      banner
-      className={'text-center font-medium w-full'}
-      message={title}
-      description={content}
-      type={variant}
-      closable
-    />
+    <>
+      {visible && (
+        <Alert
+          banner
+          className={'text-center font-medium w-full'}
+          message={title}
+          description={content}
+          type={variant}
+          closable
+          onClose={handleClose}
+        />
+        )
+      }
+    </>
   );
 }
