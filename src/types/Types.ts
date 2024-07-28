53b1172e710b5efe3377535435facb8ee9abbbee
Meas Sotheareach <sotheach1000@gmail.com>
2024-07-28 21:55:35 +0700

    update type

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -5,33 +5,49 @@ export type Pokemon = {
   name: string;
 };
 
+export interface Author {
+  profileImage?: string | StaticImport;
+  userName: string;
+}
+
 export interface ContentsTypeProps {
   slug: string;
-  thumbnail?: string;
+  thumbnail?: string | StaticImport | undefined;
   summary: string;
   blogTitle: string;
   countViewer: number;
   minRead?: number;
   published?: boolean;
-  author: {
-    profileImage?: string | StaticImport;
-    userName: string;
-  };
+  author: Author;
   createdAt?: string;
 }
+
+export interface Option {
+  option?: "grid" | "column";
+}
+
+export interface AdsProps {
+  id: number;
+  title: string;
+  imageUrl: string | undefined;
+  link: string;
+}
+
+export type BlogsProps = AdsProps | ContentsTypeProps;
 
 export interface BlogDetailsProps {
   slug: string;
   blogContent: string;
   summary: string;
-  thumbnail?: string;
+  thumbnail?: string | StaticImport | undefined;
   blogTitle: string;
   countViewer: number;
   minRead: number;
   published: boolean;
   author: {
     profileImage?: string | StaticImport;
-    userName: string;
+    userName?: string;
+    bio: string;
   };
   createdAt: string;
 }
@@ -77,18 +93,50 @@ export type ProductCardProps = {
   stocked?: number;
 };
 
-export interface RegisterAuthForm {
-  userName: string;
-  email: string;
-  password: string;
-  passwordConfirmation: string;
+export interface RecentPostProps {
+  createdAt: string;
+  timeAgo: string;
+  author: {
+    userName: string;
+  };
+  slug: string;
+  blogTitle: string;
 }
 
-export interface RegisterAuthForm {
-  userName: string;
+export interface TopAuthorProps {
+  username: string;
+  bio: string | null;
+  totalViews: 4750;
+  profileImage: StaticImport | string;
+}
+
+export interface AuthorDetailsProps {
+  id: number;
   email: string;
-  password: string;
-  passwordConfirmation: string;
+  userName: string;
+  bio: string;
+  profileImage: string | StaticImport | undefined;
+  totalViews: 0;
+  top3Count: number;
+  about: string;
+}
+
+export interface BlogByAuthors {
+  id: number;
+  blogTitle: string;
+  published: boolean;
+  slug: string;
+  isPin: boolean;
+  countViewer: number;
+  thumbnail: string | StaticImport;
+  summary: string;
+  minRead: number;
+  isDeleted: boolean;
+  createdAt: string;
+  author: {
+    profileImage: string | StaticImport | undefined;
+    userName: string;
+  };
 }
 
 export interface RegisterAuthForm {
